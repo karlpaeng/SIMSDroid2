7b2082231e51f705deec6ce16210ffd6cb7a2285
karlpaeng <112166240+karlpaeng@users.noreply.github.com>
2023-03-19 18:21:43 +0800

    updated db structure

--- app/src/main/java/com/simsdroid/app/DBHelper.java
+++ app/src/main/java/com/simsdroid/app/DBHelper.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(@Nullable Context context) {
         super(context, "SariSari.db", null, 1);
@@ -29,6 +31,7 @@ public class DBHelper extends SQLiteOpenHelper {
                 "date TEXT, " +
                 "time TEXT, " +
                 "product_name TEXT, " +
+                "product_id INTEGER, " +
                 "retail_price TEXT, " +
                 "amount INTEGER, " +
                 "amountxprice TEXT)"
@@ -46,6 +49,9 @@ public class DBHelper extends SQLiteOpenHelper {
                 "store_name TEXT, " +
                 "pin INTEGER)"
         );
+        sqLiteDatabase.execSQL("CREATE TABLE order_numbers (" +
+                "order_number INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "total TEXT)");
     }
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
@@ -97,6 +103,8 @@ public class DBHelper extends SQLiteOpenHelper {
         cv.put("last_update", product.lastUpdate);
 
         long i = db.insert("products", null, cv);
+
+        db.close();
     }
     public void updateProduct(long id, ModelProducts product){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -107,9 +115,112 @@ public class DBHelper extends SQLiteOpenHelper {
                 "cost = '" + product.cost + "', " +
                 "retail_price = '" + product.retailPrice + "', "+
                 "amount = " + product.amountStock + ", " +
-                "last_update = '" + product.lastUpdate + "'" +
+                "last_update = '" + product.lastUpdate + "' " +
                 "WHERE prod_id = " + id + ";", null);
         db.close();
+    }
+    //inv value and retail value, profit
+
+    public void createOrder(ArrayList<ModelOrders> orders){ //subtract from amount in stock, create records in orders table
+        SQLiteDatabase db = this.getWritableDatabase();
+        double total = 0;
+        for (ModelOrders order : orders) {
+            ContentValues cv = new ContentValues();
+
+            cv.put("order_number", order.orderNumber);
+            cv.put("date", order.date);
+            cv.put("time", order.time);
+            cv.put("product_name", order.productName);
+            cv.put("product_id", order.productId);
+            cv.put("retail_price", order.retailPrice);
+            cv.put("amount", order.amount);
+            cv.put("amountxprice", order.amountXprice);
+
+            long i = db.insert("orders", null, cv);
+
+            Cursor cursor = db.rawQuery("SELECT amount_stock FROM products WHERE prod_id = " + order.productId + ";", null);
+            cursor.moveToFirst();
+            int tempAmt = cursor.getInt(0);
+
+            cursor.close();
+
+            tempAmt = tempAmt - order.amount;
+
+            if (tempAmt < 0) tempAmt = 0;
+
+            db.execSQL("UPDATE products SET amount_stock = " + tempAmt + " WHERE prod_id = " + order.productId + ";");
+
+            total += order.amountXprice;
+        }
+        //total
+        db.execSQL("UPDATE order_numbers SET total = " + total + " WHERE order_number = " + orders.get(0).orderNumber + ";");
+        db.close();
+    }
+
+    public long getOrderNumber(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("total", "");
+
+        long i = db.insert("order_numbers", null, cv);
+
+        Cursor cursor = db.rawQuery("SELECT order_number FROM order_numbers ORDER BY order_number DESC LIMIT 1;", null);
+        cursor.moveToFirst();
+
+        long returnLong = cursor.getLong(0);
+
+        cursor.close();
+        db.close();
+
+        return returnLong;
+    }
+
+    public ArrayList<ModelOrders> getOrderInfo(long orderNumber){
+        ArrayList<ModelOrders> ordersList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM orders WHERE order_number = " + orderNumber + ";", null);
+        if (cursor.moveToFirst()){
+            do{
+                int orderNum = cursor.getInt(1);
+                String date = cursor.getString(2);
+                String time = cursor.getString(3);
+                String productName = cursor.getString(4);
+                int productId = cursor.getInt(5);
+                String retailPrice = cursor.getString(6);
+                int amount = cursor.getInt(7);
+                String amountXprice = cursor.getString(8);
+
+                ModelOrders order = new ModelOrders(
+                        orderNum,
+                        date,
+                        time,
+                        productName,
+                        productId,
+                        Double.parseDouble(retailPrice),
+                        amount,
+                        Double.parseDouble(amountXprice)
+                );
+
+                ordersList.add(order);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return ordersList;
+    }
+
+    public double getOrderTotal(long orderNum){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        Cursor cursor = db.rawQuery("SELECT total FROM order_numbers WHERE order_number = " + orderNum + ";", null);
+        cursor.moveToFirst();
+
+        double returnDoub = Double.parseDouble(cursor.getString(0));
+
+        cursor.close();
+        db.close();
+
+        return returnDoub;
     }
 
 }
--- app/src/main/java/com/simsdroid/app/ModelOrders.java
+++ app/src/main/java/com/simsdroid/app/ModelOrders.java
@@ -5,15 +5,17 @@ public class ModelOrders {
     public String date;
     public String time;
     public String productName;
+    public long productId;
     public double retailPrice;
     public int amount;
     public double amountXprice;
 
-    public ModelOrders(long orderNumber, String date, String time, String productName, double retailPrice, int amount, double amountXprice) {
+    public ModelOrders(long orderNumber, String date, String time, String productName, long productId, double retailPrice, int amount, double amountXprice) {
         this.orderNumber = orderNumber;
         this.date = date;
         this.time = time;
         this.productName = productName;
+        this.productId = productId;
         this.retailPrice = retailPrice;
         this.amount = amount;
         this.amountXprice = amountXprice;
@@ -29,6 +31,7 @@ public class ModelOrders {
                 ", date='" + date + '\'' +
                 ", time='" + time + '\'' +
                 ", productName='" + productName + '\'' +
+                ", productId=" + productId +
                 ", retailPrice=" + retailPrice +
                 ", amount=" + amount +
                 ", amountXprice=" + amountXprice +
